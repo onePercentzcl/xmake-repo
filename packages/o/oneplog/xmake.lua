988e7129c02f38a2983769f4e79ef053f799aec7
OnePercent <onepercentzcl@gmail.com>
2026-02-24 11:27:28 +0800

    oneplog: update to v0.2.0

--- packages/o/oneplog/xmake.lua
+++ packages/o/oneplog/xmake.lua
@@ -1,17 +1,15 @@
 package("oneplog")
     set_homepage("https://github.com/onePercentzcl/oneplog")
-    set_description("High performance C++17 multi-process logging system / 高性能 C++17 多进程日志系统")
+    set_description("High performance C++17 logging system")
     set_license("MIT")
     
     add_urls("https://github.com/onePercentzcl/oneplog/archive/refs/tags/$(version).tar.gz",
              "https://github.com/onePercentzcl/oneplog.git")
-    add_versions("v0.1.3", "")  -- SHA256 需要在发布后获取
-    add_versions("v0.1.0", "")  -- SHA256 需要在发布后获取
-    add_versions("v0.0.1", "")  -- SHA256 需要在发布后获取
+    add_versions("v0.2.0", "b2d02191711d9919d9330d03c000c425aa0a1c05de831d029e55b2aa17d62f36")
     
     add_configs("shared", {description = "Build shared library", default = false, type = "boolean"})
-    add_configs("header_only", {description = "Use header-only mode", default = false, type = "boolean"})
-    add_configs("use_fmt", {description = "Use bundled fmt library", default = true, type = "boolean"})
+    add_configs("header_only", {description = "Header-only mode", default = true, type = "boolean"})
+    add_configs("use_fmt", {description = "Use fmt library", default = true, type = "boolean"})
     
     if is_plat("linux") then
         add_syslinks("pthread", "rt")
@@ -20,23 +18,30 @@ package("oneplog")
     end
     
     on_install(function (package)
-        local configs = {}
-        if package:config("shared") then
-            configs.kind = "shared"
-        elseif package:config("header_only") then
-            configs.kind = "headeronly"
+        local configs = {"-DONEPLOG_BUILD_TESTS=OFF", "-DONEPLOG_BUILD_EXAMPLES=OFF"}
+        
+        if package:config("header_only") then
+            table.insert(configs, "-DONEPLOG_HEADER_ONLY=ON")
+        else
+            table.insert(configs, "-DONEPLOG_HEADER_ONLY=OFF")
+            if package:config("shared") then
+                table.insert(configs, "-DONEPLOG_BUILD_SHARED=ON")
+            end
         end
         
-        import("package.tools.xmake").install(package, configs)
+        if package:config("use_fmt") then
+            table.insert(configs, "-DONEPLOG_USE_FMT=ON")
+        end
+        
+        import("package.tools.cmake").install(package, configs)
     end)
     
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include <oneplog/oneplog.hpp>
+            #include <oneplog/logger.hpp>
             void test() {
-                oneplog::Init();
-                log::Info("test");
-                oneplog::Shutdown();
+                oneplog::SyncLogger logger;
+                logger.Info("test");
             }
         ]]}, {configs = {languages = "c++17"}}))
     end)
