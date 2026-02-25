23dd6a073d47d276b82f1bdf325455bf902d9fbd
OnePercent <onepercentzcl@gmail.com>
2026-02-26 01:06:10 +0800

    oneplog: add v0.3.0

--- packages/o/oneplog/xmake.lua
+++ packages/o/oneplog/xmake.lua
@@ -1,15 +1,18 @@
 package("oneplog")
     set_homepage("https://github.com/onePercentzcl/oneplog")
-    set_description("High performance C++17 logging system")
+    set_description("High performance C++17 multi-process logging system / 高性能 C++17 多进程日志系统")
     set_license("MIT")
     
     add_urls("https://github.com/onePercentzcl/oneplog/archive/refs/tags/$(version).tar.gz",
              "https://github.com/onePercentzcl/oneplog.git")
-    add_versions("v0.2.1", "745c0f8a1f321368cde93c8e7667408d9b6a3de148daf524c51aca28908997b0")
-    add_versions("v0.2.0", "b2d02191711d9919d9330d03c000c425aa0a1c05de831d029e55b2aa17d62f36")
+    add_versions("v0.3.0", "42a70c45503f167eb06360ed02855729a515bf589b9a3a2f341790e7916a5100")
+    add_versions("v0.1.3", "")  -- SHA256 需要在发布后获取
+    add_versions("v0.1.0", "")  -- SHA256 需要在发布后获取
+    add_versions("v0.0.1", "")  -- SHA256 需要在发布后获取
     
     add_configs("shared", {description = "Build shared library", default = false, type = "boolean"})
-    add_configs("header_only", {description = "Header-only mode", default = true, type = "boolean"})
+    add_configs("header_only", {description = "Use header-only mode", default = false, type = "boolean"})
+    add_configs("use_fmt", {description = "Use bundled fmt library", default = true, type = "boolean"})
     
     if is_plat("linux") then
         add_syslinks("pthread", "rt")
@@ -18,26 +21,23 @@ package("oneplog")
     end
     
     on_install(function (package)
-        local configs = {"-DONEPLOG_BUILD_TESTS=OFF", "-DONEPLOG_BUILD_EXAMPLES=OFF"}
-        
-        if package:config("header_only") then
-            table.insert(configs, "-DONEPLOG_HEADER_ONLY=ON")
-        else
-            table.insert(configs, "-DONEPLOG_HEADER_ONLY=OFF")
-            if package:config("shared") then
-                table.insert(configs, "-DONEPLOG_BUILD_SHARED=ON")
-            end
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        elseif package:config("header_only") then
+            configs.kind = "headeronly"
         end
         
-        import("package.tools.cmake").install(package, configs)
+        import("package.tools.xmake").install(package, configs)
     end)
     
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
-            #include <oneplog/logger.hpp>
+            #include <oneplog/oneplog.hpp>
             void test() {
-                oneplog::SyncLogger logger;
-                logger.Info("test");
+                oneplog::Init();
+                log::Info("test");
+                oneplog::Shutdown();
             }
         ]]}, {configs = {languages = "c++17"}}))
     end)
